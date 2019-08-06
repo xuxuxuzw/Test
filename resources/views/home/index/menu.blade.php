c3ec65c0b35196870504788b50ae4ab566edb185
xuzhaowen <yangzeloveyou@gmail.com>
2019-08-06 21:40:16 +0800

    初始化微信公众号菜单设置

--- resources/views/home/index/menu.blade.php
+++ resources/views/home/index/menu.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{!! csrf_token() !!}"/>
+    
     <title>jQuery仿微信公众号自定义菜单界面操作代码</title>
 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
