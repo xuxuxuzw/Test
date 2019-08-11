54662bf314e55385b844f06a5415ceec4083cace
xuzhaowen <yangzeloveyou@gmail.com>
2019-08-11 21:09:24 +0800

    线上测试

--- app/Modules/Common/Routes/web.php
+++ app/Modules/Common/Routes/web.php
@@ -25,5 +25,5 @@ Route::group(['prefix' => 'common'], function () {
     Route::any('msg/testAddQqSms','MsgContentControllers@testAddQqSms');
 
     //登录
-    Route::any('login/login','MsgContentControllers@send');
+    Route::any('login/login','LoginControllers@login');
 });
