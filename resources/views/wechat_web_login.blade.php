12040ab141cac54e7e464a74358aa7f1340789fd
xuzhaowen <yangzeloveyou@gmail.com>
2019-08-11 20:35:32 +0800

    线上测试

--- resources/views/wechat_web_login.blade.php
+++ resources/views/wechat_web_login.blade.php
@@ -1,5 +1,28 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <web-login></web-login>
-@endsection
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>扫码登录</title>
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+    <!-- Scripts -->
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <main class="py-4">
+        <web-login></web-login>
+    </main>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
