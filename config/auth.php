93ddf0cf4d93bbbd99ab0b4a4c7329c9e18f1de1
xuzhaowen <yangzeloveyou@gmail.com>
2019-08-11 09:17:15 +0800

    线上测试

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Common\Models\User::class,
         ],
 
         // 'users' => [
