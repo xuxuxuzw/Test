717abdb178f880906d040d81c60c137d9e3dcd23
xuzhaowen <yangzeloveyou@gmail.com>
2019-08-11 21:05:24 +0800

    线上测试

---
+++ app/Modules/Common/Resources/Views/login/login.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel</title>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div style="text-align: center">
+            <img src="/images/c43e884352f59a4d23574aebaeb97f3.png" alt=""><br>
+            <a href="{{route('wechat.web.login')}}" style="font-size: 30px"> 测试微信扫码登录</a>
+            @include('wechat_web_login.blade')
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/wechat_web_login.blade.php
+++ resources/views/wechat_web_login.blade.php
@@ -1,28 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>扫码登录</title>
-    <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
-    <!-- Scripts -->
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+<!-- CSRF Token，如果单独页面的话就要加上 -->
+{{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
+{{--<title>扫码登录</title>--}}
+<script>
+    window.Laravel = <?php echo json_encode([
+        'csrfToken' => csrf_token(),
+    ]); ?>
+</script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <div id="app">
     <main class="py-4">
         <web-login></web-login>
     </main>
 </div>
 <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
